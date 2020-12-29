46213189197c45e427f4e35da4c831188ec2f1af
Miha Jakovac <miha@mjc.si>
2020-12-29 16:39:55 +0100

    changed IP for Ping test

--- src/QAToolKit.Engine.Probes.Test/Probes/PingProbeTests.cs
+++ src/QAToolKit.Engine.Probes.Test/Probes/PingProbeTests.cs
@@ -23,7 +23,7 @@ namespace QAToolKit.Engine.Probes.Test.Probes
         [Fact]
         public async Task PingHostNameTest_Success()
         {
-            var pinger = new PingProbe("github.com", 500);
+            var pinger = new PingProbe("52.217.36.203", 500);
             var result = await pinger.Execute();
 
             _logger.LogInformation($"Success: {result.Success}");
